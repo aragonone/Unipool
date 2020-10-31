edc6845ebdb10e848a1bba4489be94dabef8c85f
Brett Sun <qisheng.brett.sun@gmail.com>
2020-10-31 22:54:12 +0100

    Oct: fix initiator being allowed to be re-initiated

--- packages/oct/README.md
+++ packages/oct/README.md
@@ -9,3 +9,4 @@ Deployed addresses:
 - ANT/ETH UNI-V2 Pool: [`0x37B7870148b4B815cb6A4728a84816Cc1150e3aa`](https://etherscan.io/address/0x37b7870148b4b815cb6a4728a84816cc1150e3aa)
 - ANT/USDC 80:20 BPT Pool: [`0x7F2b9E4134Ba2f7E99859aE40436Cbe888E86B79`](https://etherscan.io/address/0x7f2b9e4134ba2f7e99859ae40436cbe888e86b79)
 - RewardsInitiator: [`0xCF5C5efD05c47C3a3670C3E57E3ea0F16800dB9C`](https://etherscan.io/address/0xcf5c5efd05c47c3a3670c3e57e3ea0f16800db9c)
+  - Note that this initiator was later revoked. It does not contain the same source as the current version in this repo, which was updated retroactively to prevent multiple initiations.
--- packages/oct/contracts/RewardsInitiator.sol
+++ packages/oct/contracts/RewardsInitiator.sol
@@ -6,6 +6,7 @@ import "./lib/IRewardDistributionRecipient.sol";
 
 contract RewardsInitiator {
     string constant private ERROR_TOO_EARLY = "REWARDS_CTRL:TOO_EARLY";
+    string constant private ERROR_ALREADY_INITIATED = "REWARDS_CTRL:ALREADY_INITIATED";
 
     uint256 constant public earliestStartTime = 1603983600; // 2020-10-29 15:00 UTC
 
@@ -13,14 +14,19 @@ contract RewardsInitiator {
     IRewardDistributionRecipient public uniPool = IRewardDistributionRecipient(0x37B7870148b4B815cb6A4728a84816Cc1150e3aa);
     IRewardDistributionRecipient public bptPool = IRewardDistributionRecipient(0x7F2b9E4134Ba2f7E99859aE40436Cbe888E86B79);
 
+    bool initiated;
+
     function initiate() external {
         require(block.timestamp >= earliestStartTime, ERROR_TOO_EARLY);
+        require(!initiated, ERROR_ALREADY_INITIATED);
 
         uint256 uniRewardBalance = poolRewardBalance(uniPool);
         uniPool.notifyRewardAmount(uniRewardBalance);
 
         uint256 bptRewardBalance = poolRewardBalance(bptPool);
         bptPool.notifyRewardAmount(bptRewardBalance);
+
+        initiated = true;
     }
 
     function poolRewardBalance(IRewardDistributionRecipient _pool) public view returns (uint256) {
